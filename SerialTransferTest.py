4c69cba5b4fcb4062a051542dafd24de1ca3e509
Dennis Rossman <dmrossman@earthlink.net>
2025-02-24 15:34:06 -0800

    Sync local to repo

--- SerialTransferTest.py
+++ SerialTransferTest.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jan  5 13:23:05 2025
-
-@author: dmros
-"""
-
 import time
 from pySerialTransfer import pySerialTransfer as txfer
 
@@ -16,24 +9,25 @@ if __name__ == '__main__':
         link.open()
         time.sleep(2) # allow some time for the Arduino to completely reset
         
-        arrData = ''
-        
         while True:
             send_size = 0
             
             ###################################################################
             # Send a list
             ###################################################################
-            # list_ = [1, 3]
+            # list_ = [41, 42]
             # list_size = link.tx_obj(list_)
             # send_size += list_size
             
             ###################################################################
             # Send a string
             ###################################################################
+            # bytes_ = b'hello'
+            bytes_ = [b'\x00', b'\x1F', b'\x9F', b'\xAF', b'\xFF']
             # str_ = 'hello'
-            # str_size = link.tx_obj(str_, send_size) - send_size
-            # send_size += str_size
+            str_ = str(bytes_)
+            str_size = link.tx_obj(str_, send_size) - send_size
+            send_size += str_size
             
             ###################################################################
             # Send a float
@@ -42,21 +36,17 @@ if __name__ == '__main__':
             # float_size = link.tx_obj(float_, send_size) - send_size
             # send_size += float_size
             
-            # int_ = 13
-            # int_size = link.tx_obj(int_)
-            # send_size += int_size
-            # print('Send size:')
-            # print(send_size)
             ###################################################################
             # Transmit all the data to send in a single packet
             ###################################################################
-            # link.send(send_size, 27) 
+            link.send(send_size)
             
-            ##################################################### ##############
+            ###################################################################
             # Wait for a response and report any errors while receiving packets
             ###################################################################
             while not link.available():
                 # A negative value for status indicates an error
+                 # if link.status.value < 0:
                 if link.status < 0:
                     if link.status == txfer.Status.CRC_ERROR:
                         print('ERROR: CRC_ERROR')
@@ -66,9 +56,6 @@ if __name__ == '__main__':
                         print('ERROR: STOP_BYTE_ERROR')
                     else:
                         print('ERROR: {}'.format(link.status.name))
-            
-            arrData = link.rx_obj(obj_type=str, start_pos=0, obj_byte_size=1)
-            print(arrData)
             
             ###################################################################
             # Parse response list
@@ -80,9 +67,9 @@ if __name__ == '__main__':
             ###################################################################
             # Parse response string
             ###################################################################
-            # rec_str_   = link.rx_obj(obj_type=type(str_),
-            #                         obj_byte_size=str_size,
-            #                         start_pos=list_size)
+            rec_str_   = link.rx_obj(obj_type=type(str_))
+            #                         obj_byte_size=str_size)
+            #                         start_pos=0)
             
             ###################################################################
             # Parse response float
@@ -96,11 +83,10 @@ if __name__ == '__main__':
             ###################################################################
             # print('SENT: {} {} {}'.format(list_, str_, float_))
             # print('RCVD: {} {} {}'.format(rec_list_, rec_str_, rec_float_))
-            # print(' ')
-            # print('SENT: {}'.format(int_))
-            # print('RCVD: {}'.format(rec_int_))
-            # print(' ')
-    
+            print('SENT: {}'.format(str_))
+            print('RCVD: {}'.format(rec_str_))
+            print(' ')
+            
     except KeyboardInterrupt:
         try:
             link.close()
