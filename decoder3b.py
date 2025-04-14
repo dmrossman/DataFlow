c805fe6371adc6e0956ccc0a79eba20118a4b947
Dennis Rossman <dmrossman@earthlink.net>
2025-04-14 09:21:52 -0700

    Updated decoder3b - added scaler

--- decoder3b.py
+++ decoder3b.py
@@ -25,53 +25,53 @@ class fileDecoder():
                      'Accel Axis 3', 'Accel Supp I', 'Accel Supp V', 'E.S. Primary I', 'E.S. Secondary I', 'Gas Leak Vlv 1', 'Gas Leak Vlv 2', 'Gas Leak Vlv 3', 
                      'Gas Leak Vlv 4', 'Plus Ten 1', 'Plus Ten 2', 'Plus Ten 3', 'Ground', 'DoseX', 'DoseY']
         
-        # Format is 'Value Name' : [position number, row, column, first byte in message, last byte in message, K1, K2], ... 
-        self.lookup = {'Dose'              : [2, 0, 1, 6, 10, 2, 64, 'E'], 
-                       'Beam Current'      : [3, 1, 1, 10, 14, 0.002, 54, 'F'], 
-                       'Wafer Size'        : [4, 2, 1, 14, 18, 2, 64, 'D'], 
-                       'Preset Scans'      : [5, 3, 1, 18, 22, 2, 64, 'D'], 
-                       'Estimated Time'    : [6, 4, 1, 22, 26, 0.002, 59, 'F'], 
-                       'Actual Time'       : [7, 5, 1, 26, 30, 0.002, 59, 'F'], 
-                       'Press Comp'        : [8, 6, 1, 30, 34, 0.002, 59, 'F'], 
-                       'Trim'              : [9, 7, 1, 34, 38, 0.002, 59, 'F'],
-                       'Beam Energy'       : [10, 8, 1, 6, 10, 0.032, 66, 'F'], 
-                       'A.M.U.'            : [11, 9, 1, 10, 14, 0.03125038, 61, 'F'], 
-                       'Magnet Current'    : [12, 10, 1, 14, 18, 1.333, 64, 'F'], 
-                       'Pressure P1'       : [13, 11, 1, 6, 10, 2, 64, 'E'],
-                       'Pressure P2'       : [14, 12, 1, 10, 14, 2, 64, 'E'], 
-                       'Pressure P3'       : [15, 13, 1, 14, 18, 2, 64, 'E'], 
-                       'E.S. Press Start'  : [16, 14, 1, 18, 22, 2, 64, 'E'], 
-                       'E.S. Press Stop'   : [17, 15, 1, 22, 26, 2, 64, 'E'],
-                       'Source Arc I'      : [18, 16, 1, 6, 10, 0.002, 59, 'F'],
-                       'Source Arc V'      : [19, 17, 1, 38, 42, 0.002, 59, 'F'], 
-                       'Source Fil I'      : [20, 18, 1, 10, 14, 0.002, 59, 'F'], 
-                       'Source Fil V'      : [21, 19 ,1, 42, 46, 0.002, 59, 'F'],
-                       'Source Mag I'      : [22, 20, 1, 70, 74 , 0.002, 59, 'F'], 
-                       'Vaporizer'         : [23, 21, 1, 198, 202, 0.002, 59, 'V'], # This one will need to be fixed
-                       'Vaporizer Oven'    : [24, 22, 1, 62, 66, 0.002, 59, 'F'], 
-                       'Vaporizer Heater'  : [25, 0, 1, 66, 70, 0.002, 59, 'F'],
-                       'Extraction I'      : [26, 1, 1, 114, 118, 0.002, 54, 'F'], 
-                       'Extraction V'      : [27, 2, 1, 74, 78, 0.002, 64, 'F'], 
-                       'E.S. Aperture V'   : [28, 3, 1, 138, 142, 0.002, 64, 'F'],   # I found it.  Need to verify the values
-                       'Extraction Axis 1' : [29, 4, 1, 78, 82, 2, 64, 'D'], 
-                       'Extraction Axis 2' : [30, 5, 1, 82, 86, 2, 64, 'D'], 
-                       'Extraction Axis 3' : [31, 6, 1, 86, 90, 2, 64, 'D'], 
-                       'Ext Suppress I'    : [32, 7, 1, 126, 130, 0.002, 54, 'F'], 
-                       'Ext Suppress V'    : [33, 8, 1, 130, 134, 0.002, 64, 'F'],
-                       'Acceleration I'    : [34, 9, 1, 178, 182, 0.002, 54, 'F'], 
-                       'Accel Axis 3'      : [35, 10, 1, 150, 154, 2, 64, 'D'], 
-                       'Accel Supp I'      : [36, 11, 1, 190, 194, 0.002, 54, 'F'], 
-                       'Accel Supp V'      : [37, 12, 1, 194, 198, 0.002, 64, 'F'],
-                       'E.S. Primary I'    : [38, 13, 1, 174, 178, 0.002, 54, 'F'], 
-                       'E.S. Secondary I'  : [39, 14, 1, 166, 170, 0.002, 54, 'F'], 
-                       'Gas Leak Vlv 1'    : [40, 15, 1, 14, 18, 2, 64, 'D'], 
-                       'Gas Leak Vlv 2'    : [41, 16, 1, 18, 22, 2, 64, 'D'],
-                       'Gas Leak Vlv 3'    : [42, 17, 1, 22, 26, 2, 64, 'D'], 
-                       'Gas Leak Vlv 4'    : [43, 18, 1, 26, 30, 2, 64, 'D'], 
-                       'Plus Ten 1'        : [44, 19, 1, 54, 58, 2, 64, 'D'], 
-                       'Plus Ten 2'        : [45, 20, 1, 118, 122, 2, 64, 'D'], 
-                       'Plus Ten 3'        : [46, 21, 1, 182, 186, 2, 64, 'D'], 
-                       'Ground'            : [47, 22, 1, 58, 62, 2, 64, 'D'],
+        # Format is 'Value Name' : [position number, row, column, first byte in message, last byte in message, scaler, format]
+        self.lookup = {'Dose'              : [2, 0, 1, 6, 10, 1, 'E'], 
+                       'Beam Current'      : [3, 1, 1, 10, 14, 1000, 'F'], 
+                       'Wafer Size'        : [4, 2, 1, 14, 18, 1, 'D'], 
+                       'Preset Scans'      : [5, 3, 1, 18, 22, 1, 'D'], 
+                       'Estimated Time'    : [6, 4, 1, 22, 26, 1, 'F'], 
+                       'Actual Time'       : [7, 5, 1, 26, 30, 1, 'F'], 
+                       'Press Comp'        : [8, 6, 1, 30, 34, 1, 'F'], 
+                       'Trim'              : [9, 7, 1, 34, 38, 1, 'F'],
+                       'Beam Energy'       : [10, 8, 1, 6, 10, 0.001, 'F'], 
+                       'A.M.U.'            : [11, 9, 1, 10, 14, 1, 'F'], 
+                       'Magnet Current'    : [12, 10, 1, 14, 18, 1000, 'F'], 
+                       'Pressure P1'       : [13, 11, 1, 6, 10, 1, 'E'],
+                       'Pressure P2'       : [14, 12, 1, 10, 14, 1, 'E'], 
+                       'Pressure P3'       : [15, 13, 1, 14, 18, 1, 'E'], 
+                       'E.S. Press Start'  : [16, 14, 1, 18, 22, 1, 'E'], 
+                       'E.S. Press Stop'   : [17, 15, 1, 22, 26, 1, 'E'],
+                       'Source Arc I'      : [18, 16, 1, 6, 10, 1, 'F'],
+                       'Source Arc V'      : [19, 17, 1, 38, 42, 1, 'F'], 
+                       'Source Fil I'      : [20, 18, 1, 10, 14, 1, 'F'], 
+                       'Source Fil V'      : [21, 19 ,1, 42, 46, 1, 'F'],
+                       'Source Mag I'      : [22, 20, 1, 70, 74 , 1, 'F'], 
+                       'Vaporizer'         : [23, 21, 1, 198, 202, 1, 'V'], # This one will need to be fixed
+                       'Vaporizer Oven'    : [24, 22, 1, 62, 66, 1, 'F'], 
+                       'Vaporizer Heater'  : [25, 0, 1, 66, 70, 1, 'F'],
+                       'Extraction I'      : [26, 1, 1, 114, 118, 1000, 'F'], 
+                       'Extraction V'      : [27, 2, 1, 74, 78, 0.001, 'F'], 
+                       'E.S. Aperture V'   : [28, 3, 1, 138, 142, 1, 'F'],   # I found it.  Need to verify the values
+                       'Extraction Axis 1' : [29, 4, 1, 78, 82, 1, 'D'], 
+                       'Extraction Axis 2' : [30, 5, 1, 82, 86, 1, 'D'], 
+                       'Extraction Axis 3' : [31, 6, 1, 86, 90, 1, 'D'], 
+                       'Ext Suppress I'    : [32, 7, 1, 126, 130, 1000, 'F'], 
+                       'Ext Suppress V'    : [33, 8, 1, 130, 134, 1, 'F'],
+                       'Acceleration I'    : [34, 9, 1, 178, 182, 1000, 'F'], 
+                       'Accel Axis 3'      : [35, 10, 1, 150, 154, 1, 'D'], 
+                       'Accel Supp I'      : [36, 11, 1, 190, 194, 1000, 'F'], 
+                       'Accel Supp V'      : [37, 12, 1, 194, 198, 1, 'F'],
+                       'E.S. Primary I'    : [38, 13, 1, 174, 178, 1000, 'F'], 
+                       'E.S. Secondary I'  : [39, 14, 1, 166, 170, 1000, 'F'], 
+                       'Gas Leak Vlv 1'    : [40, 15, 1, 14, 18, 1, 'D'], 
+                       'Gas Leak Vlv 2'    : [41, 16, 1, 18, 22, 1, 'D'],
+                       'Gas Leak Vlv 3'    : [42, 17, 1, 22, 26, 1, 'D'], 
+                       'Gas Leak Vlv 4'    : [43, 18, 1, 26, 30, 1, 'D'], 
+                       'Plus Ten 1'        : [44, 19, 1, 54, 58, 1, 'D'], 
+                       'Plus Ten 2'        : [45, 20, 1, 118, 122, 1, 'D'], 
+                       'Plus Ten 3'        : [46, 21, 1, 182, 186, 1, 'D'], 
+                       'Ground'            : [47, 22, 1, 58, 62, 1, 'D'],
                        'DoseX'             : [48],
                        'DoseY'             : [49]}
         
@@ -127,12 +127,12 @@ class fileDecoder():
         fields = ['Dose', 'Beam Current', 'Wafer Size', 'Preset Scans', 'Estimated Time', 'Actual Time', 'Press Comp', 'Trim']
         for field in fields:
             # Calculate the value for the field
-            position, row, col, first_byte, last_byte, K1, K2, fmt = self.lookup[field]
+            position, row, col, first_byte, last_byte, scaler, fmt = self.lookup[field]
             # Make sure the bytes to decode are in the message
             # Last byte in message is 255, so decrement by one
             if(last_byte < (len(message) - 1)):
                 # val = self.decodeValue(message[first_byte: last_byte], K1, K2, fmt)
-                val = self.makeFloat(message[first_byte: last_byte])
+                val = self.decodeValue(message[first_byte: last_byte], scaler, fmt)
                 self.buffer[position] = val
         
         if(len(message) > 40):
@@ -147,12 +147,12 @@ class fileDecoder():
         fields = ['Pressure P1', 'Pressure P2', 'Pressure P3', 'E.S. Press Start', 'E.S. Press Stop']
         for field in fields:
             # Calculate the value for the field
-            position, row, col, first_byte, last_byte, K1, K2, fmt = self.lookup[field]
+            position, row, col, first_byte, last_byte, scaler, fmt = self.lookup[field]
             # Make sure the bytes to decode are in the message
             # Last byte in message is 255, so decrement by one
             if(last_byte < (len(message) - 1)):
                 # val = self.decodeValue(message[first_byte: last_byte], K1, K2, fmt)
-                val = self.makeFloat(message[first_byte: last_byte])
+                val = self.decodeValue(message[first_byte: last_byte], scaler, fmt)
                 self.buffer[position] = val
         
     def setES(self, message):
@@ -162,12 +162,12 @@ class fileDecoder():
         fields = []
         for field in fields:
             # Calculate the value for the field
-            position, row, col, first_byte, last_byte, K1, K2, fmt = self.lookup[field]
+            position, row, col, first_byte, last_byte, scaler, fmt = self.lookup[field]
             # Make sure the bytes to decode are in the message
             # Last byte in message is 255, so decrement by one
             if(last_byte < (len(message) - 1)):
                 # val = self.decodeValue(message[first_byte: last_byte], K1, K2, fmt)
-                val = self.makeFloat(message[first_byte: last_byte])
+                val = self.decodeValue(message[first_byte: last_byte], scaler, fmt)
                 self.buffer[position] = val
         
     def setAMU(self, message):
@@ -177,12 +177,12 @@ class fileDecoder():
         fields = ['Beam Energy', 'A.M.U.', 'Magnet Current']
         for field in fields:
             # Calculate the value for the field
-            position, row, col, first_byte, last_byte, K1, K2, fmt = self.lookup[field]
+            position, row, col, first_byte, last_byte, scaler, fmt = self.lookup[field]
             # Make sure the bytes to decode are in the message
             # Last byte in message is 255, so decrement by one
             if(last_byte < (len(message) - 1)):
                 # val = self.decodeValue(message[first_byte: last_byte], K1, K2, fmt)
-                val = self.makeFloat(message[first_byte: last_byte])
+                val = self.decodeValue(message[first_byte: last_byte], scaler, fmt)
                 self.buffer[position] = val
         
     def setBeam(self, message):
@@ -196,10 +196,10 @@ class fileDecoder():
         'Gas Leak Vlv 4', 'Plus Ten 1', 'Plus Ten 2', 'Plus Ten 3', 'Ground']
         for field in fields:
             # Calculate the value for the field
-            position, row, col, first_byte, last_byte, K1, K2, fmt = self.lookup[field]
+            position, row, col, first_byte, last_byte, scaler, fmt = self.lookup[field]
             # Make sure the bytes to decode are in the message
             if(last_byte < len(message)):
-                val = self.decodeValue(message[first_byte: last_byte], K1, K2, fmt)
+                val = self.decodeValue(message[first_byte: last_byte], scaler, fmt)
                 self.buffer[position] = val
         
     def checkSum(self, message):
@@ -247,28 +247,20 @@ class fileDecoder():
                         return(True)
         return(False)
     
-    def decodeValue(self, msgBytes, K1, K2, fmt):
-        # decodeValue expects three bytes from one of the messages
-        # If the first byte is zero, just return zero
-        # Otherwise:
-        #      Raise 4 to the power of the result of subtracting K2 from the first byte
-        #      Take the second byte, if it is less than 128, then byte 2 and byte 3 don't change
-        #      If byte 2 is greater than 127, then byte 2 = byte 2 * 2 - 128
-        #      and byte 3 = byte 3 * 2
-        # result = (K1/32768) * 4^(byte 1 - K2)*(32768 + 256 * byte 2 + byte 3)
+    def decodeValue(self, msgBytes, scaler, fmt):
+        # decodeValue expects four bytes from one of the messages
+        # It uses make float to turn the four bytes into a floating point number
+        # Then it scales the amount (current in milliamps, energy in KeV, etc)
+        # Then prints out the data in the correct format
         if(len(msgBytes) == 0):
             result = 0
-        elif(msgBytes[0] == 0):
-            result = 0
         else:
-            if (msgBytes[1] >= 128):
-                msgBytes[1] = 2 * msgBytes[1] - 128
-                msgBytes[2] = 2 * msgBytes[2]
+            result = self.makeFloat(msgBytes)
             
-            result = (K1/32768) * 4**(msgBytes[0] - K2) * (32768 + (256 * msgBytes[1]) + msgBytes[2])
+        # Now scale the result
+        result = result * scaler
         
         # Now that we have the value, let's return a string with the correct format
-        
         if(fmt == 'D'):     # Deccimal
             return('{:.0f}'.format(result))
         elif(fmt == 'F'):   # Floating point
@@ -276,22 +268,24 @@ class fileDecoder():
         elif(fmt == 'E'):   # Scientific notation, exponent
             return('{:.3e}'.format(result))
         elif(fmt == 'V'):   # Vaporizer - this is special
-            if((msgBytes[0] > 63) and (msgBytes[0] < 80)):
-                return('Off')
-            elif((msgBytes[0] > 79) and (msgBytes[0] < 96)):
-                return('On')
-            elif((msgBytes[0] > 95) and (msgBytes[0] < 112)):
-                return('Cool')
+            return('{:.3f}'.format(result))
+        
+        #    if((msgBytes[0] > 63) and (msgBytes[0] < 80)):
+        #        return('Off')
+        #    elif((msgBytes[0] > 79) and (msgBytes[0] < 96)):
+        #        return('On')
+        #    elif((msgBytes[0] > 95) and (msgBytes[0] < 112)):
+        #        return('Cool')
         else:
             print('Error: Invalid number format')
             
             
-    def makeFloat(self, bytes):
+    def makeFloat(self, msgBytes):
         # This will convert four bytes to IEEE float
-        if len(bytes) != 4:
+        if len(msgBytes) != 4:
             raise ValueError("Input must be four bytes long.")
         
-        return(struct.unpack('>f', bytes)[0])
+        return(struct.unpack('>f', bytes(msgBytes))[0])
             
         
     
@@ -305,11 +299,13 @@ fd = fileDecoder()
 # fileName = 'E:/dataFlowLog4.txt'
 # fileName = 'F:/NV10 Lightpipes/DataFlow1/Mar-5-2025/dataFlowLog4=Mar5-2025.txt'
 # fileName = 'F:/NV10 Lightpipes/DataFlow1/Mar-12-2025/dataFlowLog4=Mar12-2025A.txt'
-fileName = 'C:/Users/DRossman/dataFlowLog4-Mar12-2025A.txt'
+# fileName = 'C:/Users/DRossman/dataFlowLog4-Mar12-2025A.txt'
+fileName = 'F:/NV10 Lightpipes/DataFlow1/Apr-13-2025/dataFlowLog4-Apr13-2025.txt'
 
 # outputFileName = 'C:/Users/Dmros/Downloads/DataFlow-main//DataFlow-main/output.txt'
 # outputFileName = 'E:/output.txt'
-outputFileName = 'C:/Users/DRossman/output-impOnly.txt'
+# outputFileName = 'C:/Users/DRossman/output-impOnly.txt'
+outputFileName = 'F:/NV10 Lightpipes/DataFlow1/Apr-13-2025/dataFlowLog4-Apr13-2025-out.txt'
 
 f = open(fileName)
 fo = open(outputFileName, "w")
